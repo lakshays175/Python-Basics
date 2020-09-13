5de503413a35b5468e3bfd9fb6f298c4ec452c32
lakshays175 <52139405+lakshays175@users.noreply.github.com>
2020-09-13 10:36:54 +0200

    Added a code using inheritance

---
+++ Inhertiance.py
@@ -0,0 +1,19 @@
+class Tomato(): 
+     def type(self): 
+       print("Vegetable") 
+     def color(self):
+       print("Red") 
+class Apple(): 
+     def type(self):
+       print("Fruit") 
+     def color(self): 
+       print("Red") 
+      
+def func(self): 
+       self.type() 
+       self.color()
+        
+obj_tomato = Tomato() 
+obj_apple = Apple() 
+func(obj_tomato) 
+func(obj_apple)
